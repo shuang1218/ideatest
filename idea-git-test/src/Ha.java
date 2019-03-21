61d859232234371a6f48bc4ba25f3cedca91a6c8
adminws <123@qq.com>
2019-03-21 20:43:06 +0800

    添加test工程到git

---
+++ idea-git-test/src/Ha.java
@@ -0,0 +1,10 @@
+public class Ha {
+    public static void main(String[] args) {
+
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+    }
+}
